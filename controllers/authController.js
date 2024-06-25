03ffb59eb81697345fe5748e5d0336d98c2470e1
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-06-24 23:25:19 -0300

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,79 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
+const User = require('../models/User');
+
+exports.register = (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: 'Email já existe' });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+};
+
+exports.login = (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(404).json({ emailnotfound: 'Email não encontrado' });
+        }
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+
+                jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    { expiresIn: 31556926 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                    }
+                );
+            } else {
+                return res
+                    .status(400)
+                    .json({ passwordincorrect: 'Senha incorreta' });
+            }
+        });
+    });
+};
