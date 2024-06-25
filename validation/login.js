875a2a52535d48e6dfc56805847e39cf525c0cf7
phoenixsrd <151684839+phoenixsrd@users.noreply.github.com>
2024-06-24 23:30:12 -0300

    Create login.js

---
+++ validation/login.js
@@ -0,0 +1,28 @@
+// validation/login.js
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+
+    // Convert empty fields to an empty string so we can use validator functions
+    data.email = !isEmpty(data.email) ? data.email : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+
+    // Email checks
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email é obrigatório';
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email inválido';
+    }
+
+    // Password checks
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Senha é obrigatória';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
